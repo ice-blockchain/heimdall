55fa7fc962478bb52845518c76095116ef0b46ee
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2025-01-15 16:56:38 +0300

    extra sql condition

--- accounts/DDL.sql
+++ accounts/DDL.sql
@@ -73,13 +73,15 @@ CREATE TABLE IF NOT EXISTS wallet_views (
 CREATE INDEX IF NOT EXISTS wallet_views_user_id ON wallet_views (user_id);
 
 DO $$ BEGIN
-    ALTER TABLE wallet_views ADD COLUMN IF NOT EXISTS id TEXT DEFAULT '' NOT NULL,
-        DROP CONSTRAINT IF EXISTS wallet_views_pkey;
-    UPDATE wallet_views SET id = gen_random_uuid()
-    WHERE id = '';
-    if NOT exists (select constraint_name from information_schema.table_constraints where table_name = 'wallet_views' and constraint_type = 'PRIMARY KEY') then
-        ALTER TABLE wallet_views
-            ADD CONSTRAINT wallet_views_pkey PRIMARY KEY(id);
+    if NOT exists (SELECT column_name FROM information_schema.columns WHERE table_name='wallet_views' and column_name='id') then
+        ALTER TABLE wallet_views ADD COLUMN IF NOT EXISTS id TEXT DEFAULT '' NOT NULL,
+            DROP CONSTRAINT IF EXISTS wallet_views_pkey;
+        UPDATE wallet_views SET id = gen_random_uuid()
+        WHERE id = '';
+        if NOT exists (select constraint_name from information_schema.table_constraints where table_name = 'wallet_views' and constraint_type = 'PRIMARY KEY') then
+            ALTER TABLE wallet_views
+                ADD CONSTRAINT wallet_views_pkey PRIMARY KEY(id);
+        end if;
     end if;
 END $$;
 
