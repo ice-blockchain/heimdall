c0cf160096f5796d9a25e7696b9468e454c84fec
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2025-07-20 14:49:11 +0300

    fix ddl to be consistent with staging

--- coins/DDL.sql
+++ coins/DDL.sql
@@ -1,6 +1,6 @@
 -- SPDX-License-Identifier: ice License 1.0
 CREATE TABLE IF NOT EXISTS global (
-      value BIGINT NOT NULL,
+      value TEXT NOT NULL,
       key TEXT PRIMARY KEY
 );
 
@@ -120,7 +120,7 @@ DO $$ BEGIN
                                    coingecko_coin_id = 'ice',
                                    symbol = 'ice',
                                    symbol_group = 'ice',
-                                   version = ((select global.value from global where global.key = '%[3]v') + 1);
+                                   version = ((select global.value from global where global.key = '%[3]v')::BIGINT + 1);
     -- old ice on eth
     INSERT INTO coins (sync_frequency, created_at, updated_at, data_updated_at, decimals, version, price_usd, id, coingecko_coin_id, network, name, contract_address, symbol, symbol_group, icon_url, native)
     VALUES            ('%[1]v', now(), now(), now(), 9, ((select global.value from global where global.key = '%[3]v')::BIGINT + 1), %[2]v, 'bd3ddc2a-73fc-4c24-3e9b-dbc992fef82d', 'ice', 'eth', 'Ice Open Network', '0x79f05c263055ba20ee0e814acd117c20caa10e0c', 'ice', 'ice', 'https://cdn.ice.io/online+/assets/coins/ion.svg', false)
@@ -129,7 +129,7 @@ DO $$ BEGIN
                                    coingecko_coin_id = 'ice',
                                    symbol = 'ice',
                                    symbol_group = 'ice',
-                                   version = ((select global.value from global where global.key = '%[3]v') + 1);
+                                   version = ((select global.value from global where global.key = '%[3]v')::BIGINT + 1);
     -- old ice on solana
     INSERT INTO coins (sync_frequency, created_at, updated_at, data_updated_at, decimals, version, price_usd, id, coingecko_coin_id, network, name, contract_address, symbol, symbol_group, icon_url, native)
     VALUES            ('%[1]v', now(), now(), now(), 9, ((select global.value from global where global.key = '%[3]v')::BIGINT + 1), %[2]v, '742bd2b4-6f9b-44c5-e13c-7bd8cd43e001', 'ice', 'solana', 'Ice Open Network', 'E9aPbhb5xRVGP2L6qJixfJC5qWAzECpUFUxnGx3wUiND', 'ice', 'ice', 'https://cdn.ice.io/online+/assets/coins/ion.svg', false)
@@ -138,7 +138,7 @@ DO $$ BEGIN
                                    coingecko_coin_id = 'ice',
                                    symbol = 'ice',
                                    symbol_group = 'ice',
-                                   version = ((select global.value from global where global.key = '%[3]v') + 1);
+                                   version = ((select global.value from global where global.key = '%[3]v')::BIGINT + 1);
     -- old ice on arbitrum
     INSERT INTO coins (sync_frequency, created_at, updated_at, data_updated_at, decimals, version, price_usd, id, coingecko_coin_id, network, name, contract_address, symbol, symbol_group, icon_url, native)
     VALUES            ('%[1]v', now(), now(), now(), 9, ((select global.value from global where global.key = '%[3]v')::BIGINT + 1), %[2]v, 'bce57663-8fb0-6d41-c6f1-c03c0c39a3ad', 'ice', 'arbitrum', 'Ice Open Network', '0xAB8EBCC9eecc20Bd30c7b75c7b4e8fcCcFBf01aB', 'ice', 'ice', 'https://cdn.ice.io/online+/assets/coins/ion.svg', false)
@@ -147,7 +147,7 @@ DO $$ BEGIN
                                    coingecko_coin_id = 'ice',
                                    symbol = 'ice',
                                    symbol_group = 'ice',
-                                   version = ((select global.value from global where global.key = '%[3]v') + 1);
+                                   version = ((select global.value from global where global.key = '%[3]v')::BIGINT + 1);
     -- snow
     INSERT INTO coins (sync_frequency, created_at, updated_at, data_updated_at, decimals, version, price_usd, id, coingecko_coin_id, network, name, contract_address, symbol, symbol_group, icon_url, native)
     VALUES            ('%[1]v', now(), now(), now(), 9, ((select global.value from global where global.key = '%[3]v')::BIGINT + 1), 0.0000000005294336947, 'ca5a9f29-06ef-39a2-09ef-3d3942207110', 'snowman', 'eth', 'Snowman', '0xd1f3d2f5c12a205fc912358878b089eae48a557f', 'snow', 'snowman', 'https://cdn.ice.io/online+/assets/coins/ion.svg', false)
