ca7eddcde60fa5077024d19ee7f04a61b83ecd36
ice-myles <96409608+ice-myles@users.noreply.github.com>
2025-09-26 17:11:01 +0300

    Removed p tag from verified 30009 definition.

--- accounts/verified.go
+++ accounts/verified.go
@@ -85,7 +85,6 @@ func generateVerificationEvents(heimdallPrivateKey string, masterPubKey string) 
 				{"description", verifiedBadgeDescription},
 				verifiedBadgeImage1024X1024Tag,
 				verifiedBadgeThumbnail256X256Tag,
-				{"p", masterPubKey},
 			},
 		},
 	}
