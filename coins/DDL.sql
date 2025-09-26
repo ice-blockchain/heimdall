4db54029995b6c299b0e637176e588cd00a57807
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2025-09-26 11:10:00 +0300

    fix icp network id, missing near and bob native coins

--- coins/DDL.sql
+++ coins/DDL.sql
@@ -158,4 +158,19 @@ DO $$ BEGIN
                                    symbol_group = 'snowman',
                                    version = ((select global.value from global where global.key = '%[3]v')::BIGINT + 1);
     DELETE FROM coins where network = 'ftm';
-END$$
+END$$;
+
+UPDATE coins SET
+    version = (select global.value from global where global.key = '%[3]v')::BIGINT + 1
+WHERE network = 'icp';
+
+insert into public.coins (sync_frequency, created_at, updated_at, data_updated_at, version, coingecko_coin_id, id, network, name, contract_address, symbol, symbol_group, icon_url, price_usd, decimals, native)
+values  ('0 years 0 mons 0 days 1 hours 0 mins 0.0 secs', now(), now(), now(), (select global.value from global where global.key = '%[3]v')::BIGINT + 2, 'weth', 'dff9d436-d7d5-93ad-5c2b-9d73f03e16b1', 'bob-network', 'WETH', '', 'weth', 'weth', 'https://cdn.ice.io/online+/assets/coins/weth.svg', 3921.99, 18, true),
+        ('0 years 0 mons 0 days 1 hours 0 mins 0.0 secs', now(), now(), now(), (select global.value from global where global.key = '%[3]v')::BIGINT + 2, 'near', '455e33cf-2651-920e-2f19-d8d50e9cd56d', 'near', 'NEAR Protocol', '', 'near', 'near', 'https://cdn.ice.io/online+/assets/coins/near.svg', 2.69, 24, true)
+ON CONFLICT(id) DO NOTHING;
+
+UPDATE wallet_views SET
+       coins = array_append(array_append(wallet_views.coins, ('dff9d436-d7d5-93ad-5c2b-9d73f03e16b1',null)::coin_mapping),('455e33cf-2651-920e-2f19-d8d50e9cd56d',null)::coin_mapping)
+WHERE
+    'dff9d436-d7d5-93ad-5c2b-9d73f03e16b1' NOT IN (select (unnest(wallet_views.coins)::coin_mapping).coinId) OR
+    '455e33cf-2651-920e-2f19-d8d50e9cd56d' NOT IN (select (unnest(wallet_views.coins)::coin_mapping).coinId);
