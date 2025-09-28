7cf0bfe411272da6571d3c5cb23b5f5e00b1fcc9
ice-myles <96409608+ice-myles@users.noreply.github.com>
2025-09-28 17:17:05 +0300

    Spitted increment trigger to different triggers for global_accounts and nfsw_content tables not to have version increase on ANY user cascade delete, but ONLY on cascade delete of exisint user at global_accounts/nfsw_content tables.

--- accounts/DDL.sql
+++ accounts/DDL.sql
@@ -92,8 +92,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER global_accounts_insert_version_trigger
-AFTER INSERT OR DELETE OR TRUNCATE ON global_accounts
+CREATE OR REPLACE TRIGGER global_accounts_insert_delete_version_trigger
+AFTER INSERT OR DELETE ON global_accounts
+FOR EACH ROW
+EXECUTE FUNCTION increment_global_accounts_version();
+
+CREATE OR REPLACE TRIGGER global_accounts_truncate_version_trigger
+AFTER TRUNCATE ON global_accounts
 FOR EACH STATEMENT
 EXECUTE FUNCTION increment_global_accounts_version();
 
@@ -114,8 +119,13 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER nsfw_accounts_insert_version_trigger
-AFTER INSERT OR DELETE OR TRUNCATE ON nsfw_accounts
+CREATE OR REPLACE TRIGGER nsfw_accounts_insert_delete_version_trigger
+AFTER INSERT OR DELETE ON nsfw_accounts
+FOR EACH ROW
+EXECUTE FUNCTION increment_nsfw_accounts_version();
+
+CREATE OR REPLACE TRIGGER nsfw_accounts_truncate_version_trigger
+AFTER TRUNCATE ON nsfw_accounts
 FOR EACH STATEMENT
 EXECUTE FUNCTION increment_nsfw_accounts_version();
 
