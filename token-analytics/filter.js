ab8533975c7c0cb2f3a2c657c55bb78c3d18f793
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2025-11-05 20:29:43 +0300

    fix ddl to be more normalized and support import data from qn, as it comes in json

---
+++ token-analytics/filter.js
@@ -0,0 +1,25 @@
+function main(payload) {
+    const {
+        data,
+        metadata,
+    } = payload;
+    txs = {}
+    filtered = data[0].receipts.filter(function(item) {
+        logs = item.logs.filter(function(log) {
+            // Contract addr
+            contains = log.address.toLowerCase() === '0x999bA6d981A46CB64C8A3bFFbA70101893c57aC4'.toLowerCase()
+            if (contains) {
+                txs[log.transactionHash] = true
+            }
+            return contains
+        })
+
+        return logs.length > 0
+    })
+    data[0].block.transactions = data[0].block.transactions.filter(tx => txs[tx.hash] === true)
+    return {
+        'logs': filtered.map(receipt => receipt.logs).flat(2),
+        'block': data[0].block,
+        'stream': metadata.stream_id
+    };
+}
