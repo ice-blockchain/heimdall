8bc1648e169977c048d5a019cd72da2c5a46e469
Perseus <229725321+ice-perseus@users.noreply.github.com>
2025-11-18 16:38:27 +0300

    Update DDL for QuestDB

--- token-analytics/internal/questdb/DDL.sql
+++ token-analytics/internal/questdb/DDL.sql
@@ -1,10 +1,12 @@
 -- SPDX-License-Identifier: ice License 1.0
 
 CREATE TABLE IF NOT EXISTS trades (
-                        timestamp TIMESTAMP,
-                        symbol SYMBOL,
-                        side SYMBOL,
-                        price DOUBLE,
-                        amount DOUBLE
+    timestamp TIMESTAMP,
+    pair_address VARCHAR INDEX,
+    price DECIMAL(38, 18),
+    amount DECIMAL(38, 18),
+    type SYMBOL CAPACITY 2 CACHE,
+    trader_address VARCHAR,
+    transaction_hash LONG256,
 ) TIMESTAMP(timestamp) PARTITION BY DAY WAL
-DEDUP UPSERT KEYS(timestamp, symbol);
+DEDUP UPSERT KEYS(timestamp, transaction_hash);
