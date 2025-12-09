11c5dab3f238e6ad6419262cf2779a93139f659f
ice-myles <96409608+ice-myles@users.noreply.github.com>
2025-12-09 17:08:51 +0300

    422 errors fix. Latest trades fix. Viewing session volume value fix.

--- token-analytics/latest_trades.go
+++ token-analytics/latest_trades.go
@@ -52,9 +52,9 @@ func (t *tokenAnalytics) GetLatestTrades(ctx context.Context, externalAddress st
 			COALESCE(((utp.amount / 1e18) * tokens.price_usd)::DECIMAL, 0) as balance_usd
 		FROM token_swaps 
 		JOIN tokens ON token_swaps.contract_address = tokens.contract_address
-		LEFT JOIN users creator ON creator.master_pubkey = tokens.creator_master_pubkey
-		LEFT JOIN users holder ON holder.blockchain_address = token_swaps.user_address
-		LEFT JOIN user_token_positions utp ON utp.external_address = token_swaps.external_address AND utp.master_pubkey = holder.master_pubkey
+		INNER JOIN users creator ON creator.master_pubkey = tokens.creator_master_pubkey
+		INNER JOIN users holder ON holder.blockchain_address = token_swaps.user_address
+		INNER JOIN user_token_positions utp ON utp.external_address = token_swaps.external_address AND utp.master_pubkey = holder.master_pubkey
 			WHERE tokens.external_address = $1 %[3]v
 		ORDER BY token_swaps.created_at DESC
 		LIMIT %[1]v OFFSET %[2]v
@@ -66,7 +66,7 @@ func (t *tokenAnalytics) GetLatestTrades(ctx context.Context, externalAddress st
 		}
 		return nil, time.Time{}, errors.Wrap(err, "failed to fetch latest trades")
 	}
-	trades := make([]*Trade, len(swaps))
+	trades := make([]*Trade, 0, len(swaps))
 	var maxTs time.Time
 	for i := range swaps {
 		if i == 0 {
@@ -93,13 +93,13 @@ func (t *tokenAnalytics) GetLatestTrades(ctx context.Context, externalAddress st
 		tokenAmountFloat := weiToFloat64FromBigInt(tokenAmountWeiBigInt)
 		amountUSD := tokenAmountFloat * swaps[i].PriceUSD
 
-		balanceWeiBigInt := new(big.Int)
-		if _, ok := balanceWeiBigInt.SetString(swaps[i].Balance, 10); !ok {
+		balanceFloat := new(big.Float)
+		if _, ok := balanceFloat.SetString(swaps[i].Balance); !ok {
 			log.Warn(fmt.Sprintf("failed to parse balance for swap %s: %v", swaps[i].TransactionHash, swaps[i].Balance))
 
 			continue
 		}
-		balance := weiToUint64FromBigInt(balanceWeiBigInt)
+		balance, _ := balanceFloat.Uint64()
 		creatorAddresses, err := buildAddressesFromExternalAddressAndPlatform(creatorExternalAddress, swaps[i].CreatorPlatform)
 		if err != nil {
 			log.Warn(fmt.Sprintf("failed to build creator addresses for swap %s: %v", swaps[i].TransactionHash, err))
@@ -113,7 +113,7 @@ func (t *tokenAnalytics) GetLatestTrades(ctx context.Context, externalAddress st
 			log.Warn(fmt.Sprintf("failed to build token addresses for swap %s: %v", swaps[i].TransactionHash, err))
 			tokenAddresses = Addresses{}
 		}
-		trades[i] = &Trade{
+		trades = append(trades, &Trade{
 			Creator: User{
 				Username:  swaps[i].CreatorUsername,
 				Display:   swaps[i].CreatorDisplay,
@@ -137,7 +137,7 @@ func (t *tokenAnalytics) GetLatestTrades(ctx context.Context, externalAddress st
 				Balance:    balance,
 				BalanceUSD: swaps[i].BalanceUSD,
 			},
-		}
+		})
 	}
 	return trades, maxTs, nil
 }
