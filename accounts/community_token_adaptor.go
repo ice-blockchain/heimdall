bc47327f874a62cf294076b1e9fbf9d49405a99e
Myles <96409608+ice-myles@users.noreply.github.com>
2025-12-21 20:16:34 +0300

    Fixed relay auth. (#430)

--- accounts/community_token_adaptor.go
+++ accounts/community_token_adaptor.go
@@ -4,7 +4,7 @@ package accounts
 
 import (
 	"context"
-	"math/rand"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/nbd-wtf/go-nostr"
@@ -42,7 +42,7 @@ func (a *accounts) CreateCommunityTokenAdaptor(ctx context.Context, platform, po
 	if err := communityTokenEvent.SignWithAlg(keypair.PrivateKey, model.SignAlgEDDSA, model.KeyAlgCurve25519); err != nil {
 		return nil, errors.Wrap(err, "failed to sign community token event")
 	}
-	if err := publishEventsToRelay(ctx, keypair.PrivateKey, []string{keypair.RelayURL}, []*model.Event{communityTokenEvent}); err != nil {
+	if err := publishEventsToRelay(ctx, keypair.PrivateKey, keypair.RelayURL, []*model.Event{communityTokenEvent}); err != nil {
 		return nil, errors.Wrap(err, "failed to publish community token event")
 	}
 	address := communityTokenEvent.Address()
@@ -52,55 +52,49 @@ func (a *accounts) CreateCommunityTokenAdaptor(ctx context.Context, platform, po
 	}, nil
 }
 
-func publishEventsToRelay(ctx context.Context, privateKey string, relays []string, events []*model.Event) error {
-	relay := selectRandomRelay(relays)
-	if relay == "" {
-		return nil
+func publishEventsToRelay(ctx context.Context, privateKey string, relay string, events []*model.Event) error {
+	nostrEvents := make([]*nostr.Event, len(events))
+	for i, evt := range events {
+		nostrEvents[i] = &evt.Event
 	}
-
 	nostrRelay := nostr.NewRelay(ctx, relay, nostr.WithSignatureChecker(func(e *nostr.Event) bool {
 		subzeroEvent := model.Event{Event: *e}
-		ok, _ := subzeroEvent.CheckSignature()
+		ok, err := subzeroEvent.CheckSignature()
 
-		return ok
+		return ok && err == nil
 	}))
 	if err := nostrRelay.Connect(ctx); err != nil {
 		return errors.Wrapf(err, "failed to connect to relay %s", relay)
 	}
 	defer nostrRelay.Close()
 
-	_ = nostrRelay.Publish(ctx, events[0].Event)
-	if err := nostrRelay.Auth(ctx, func(event *nostr.Event) error {
-		subZeroEvent := model.Event{Event: *event}
-		if err := subZeroEvent.SignWithAlg(privateKey, model.SignAlgEDDSA, model.KeyAlgCurve25519); err != nil {
-			return err
-		}
-		*event = subZeroEvent.Event
+	err := nostrRelay.PublishMany(ctx, nostrEvents...)
+	if err != nil {
+		if strings.Contains(err.Error(), "auth-required:") {
+			err = errors.Wrap(nostrRelay.Auth(ctx, func(event *nostr.Event) error {
+				subZeroEvent := model.Event{Event: *event}
+				if err := subZeroEvent.SignWithAlg(privateKey, model.SignAlgEDDSA, model.KeyAlgCurve25519); err != nil {
+					return err
+				}
+				*event = subZeroEvent.Event
 
-		return nil
-	}); err != nil {
-		return errors.Wrapf(err, "failed to auth to relay %s", relay)
-	}
-	nostrEvents := make([]*nostr.Event, len(events))
-	for i, evt := range events {
-		nostrEvents[i] = &evt.Event
-	}
+				return nil
+			}), "failed to authenticate to relay")
+			if err != nil {
+				return errors.Wrapf(err, "failed to auth to relay %s", relay)
+			}
+			if err := nostrRelay.PublishMany(ctx, nostrEvents...); err != nil {
+				eventIDs := make([]string, len(events))
+				for i, evt := range events {
+					eventIDs[i] = evt.Event.ID
+				}
 
-	if err := nostrRelay.PublishMany(ctx, nostrEvents...); err != nil {
-		eventIDs := make([]string, len(events))
-		for i, evt := range events {
-			eventIDs[i] = evt.Event.ID
+				return errors.Wrapf(err, "failed to publish events: %v", eventIDs)
+			}
+		} else {
+			return errors.Wrapf(err, "failed to publish event to relay %s", relay)
 		}
-		return errors.Wrapf(err, "failed to publish events: %v", eventIDs)
 	}
 
 	return nil
-}
-
-func selectRandomRelay(relays []string) string {
-	if len(relays) == 0 {
-		return ""
-	}
-
-	return relays[rand.Intn(len(relays))]
 }
