71af4a50c6ff31b97aed08a055c54dad3e0eaf0d
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2026-01-14 12:47:48 +0300

    accept 443 port as relay input also

--- relay-management/relay-management.go
+++ relay-management/relay-management.go
@@ -4,6 +4,7 @@ package relaymanagement
 
 import (
 	"context"
+	"net/url"
 
 	"github.com/goccy/go-json"
 	"github.com/pkg/errors"
@@ -20,13 +21,17 @@ func NewRelays(ctx context.Context) Relays {
 	return &r
 }
 
-func (r *relaysRepository) GetAllIONConnectRelays(ctx context.Context, requestedRelay string) ([]*UserAssignedRelay, error) {
+func (r *relaysRepository) GetAllIONConnectRelays(ctx context.Context, requestedRelay *url.URL) ([]*UserAssignedRelay, error) {
+	relayUrl4443 := *requestedRelay
+	if requestedRelay.Port() == "443" {
+		relayUrl4443.Host = requestedRelay.Hostname() + ":4443"
+	}
 	allRelays, err := storage.Select[struct {
 		IONConnectRelays []string `db:"ion_connect_relays"`
 	}](ctx, r.db, `SELECT array_agg(url) as ion_connect_relays 
 		FROM ion_connect_relays
 		WHERE region = (SELECT region FROM ion_connect_relays WHERE url = $1)
-		AND (unhealthy_started_at is NULL OR unhealthy_started_at between now()-'6 hours'::INTERVAL AND now() )`, requestedRelay)
+		AND (unhealthy_started_at is NULL OR unhealthy_started_at between now()-'6 hours'::INTERVAL AND now() )`, relayUrl4443.String())
 	if err != nil {
 		if errors.Is(err, storage.ErrNotFound) {
 			err = nil
