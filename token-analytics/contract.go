44d4ca2cf225b8fb1e12e41adc1eee984b719bb6
ice-myles <96409608+ice-myles@users.noreply.github.com>
2026-03-03 20:46:37 +0300

    Fixed trading stats expire ticker not to depend on only 1 subscription context that can be stopped forever by 1st subscriber.

--- token-analytics/contract.go
+++ token-analytics/contract.go
@@ -507,14 +507,15 @@ type (
 		onceStartTicker sync.Once
 	}
 	recentTradeStats struct {
-		stats           *TradeStats
-		initTime        int64
-		mx              sync.Mutex
-		expirations5M   *orderedmap.OrderedMap[int64, TradeStatsAggregate]
-		expirations1H   *orderedmap.OrderedMap[int64, TradeStatsAggregate]
-		expirations6H   *orderedmap.OrderedMap[int64, TradeStatsAggregate]
-		expirations24H  *orderedmap.OrderedMap[int64, TradeStatsAggregate]
-		onceStartTicker sync.Once
+		stats          *TradeStats
+		initTime       int64
+		mx             sync.Mutex
+		expirations5M  *orderedmap.OrderedMap[int64, TradeStatsAggregate]
+		expirations1H  *orderedmap.OrderedMap[int64, TradeStatsAggregate]
+		expirations6H  *orderedmap.OrderedMap[int64, TradeStatsAggregate]
+		expirations24H *orderedmap.OrderedMap[int64, TradeStatsAggregate]
+		tickerRunning  atomic.Bool
+		onExpired      atomic.Pointer[func()]
 	}
 	holderMetadata struct {
 		HolderMasterPubkey    *string `json:"holder_master_pubkey"`
